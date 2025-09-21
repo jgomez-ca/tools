74d6d71d71412ed309f43e3301c1bc0dda5e22ed
jgomez <59587212+jgomez-ca@users.noreply.github.com>
2025-09-20 20:43:59 -0400

    Add files via upload

--- redact.js
+++ redact.js
@@ -21,7 +21,7 @@ document.getElementById('redactButton').addEventListener('click', function() {
    * @returns {string} - The redacted email.
    */
 function redactEmail(email) {
-  // Split the email into local and domain parts.
+    // Split the email into local and domain parts.
   const parts = email.split("@");
   console.log('Parts -> ',parts);
   if (parts.length !== 2) {
@@ -34,43 +34,32 @@ function redactEmail(email) {
 
   // Redacts the local part.
   let redactedLocal = localPart;
-  if (localPart.length > 1) {
-    console.log('Longitud del array localPart ->', localPart.length);
-    console.log('CHARAT ->', localPart.charAt(0));
-    console.log('CHARAT - 1 ->', localPart.charAt(localPart.length - 1));
-    /*the following line redacts the email between the first and last chars
-    redactedLocal = localPart.charAt(0) + "*".repeat(localPart.length) + localPart.charAt(localPart.length - 1); */
-
-  // The folloging line redacts the email COMPLETELY
-    redactedLocal = localPart.charAt(0) + "*".repeat(localPart.length);
-    console.log('localPart redactado->', redactedLocal);
-  }
-
+  redactedLocal = "*".repeat(localPart.length);
+  
   // Separate the domain into the domain name and TLD.
   const lastDotPos = domainFull.lastIndexOf(".");
-  console.log("longitud del dominio", domainFull.length);
-  console.log("Posicion primer caracter", domainFull[0]);
-  console.log("Posicion del punto", lastDotPos[-1]);
   let domainName, tld;
   if (lastDotPos === -1) {
     domainName = domainFull;
     tld = "";
   } else {
     domainName = domainFull.substring(0, lastDotPos);
-    console.log("domainName ->", domainName);
     tld = domainFull.substring(lastDotPos); // includes the dot
-    console.log("tld ->", tld);
   }
 
-  // Redact the domain name.
-  /*let redactedDomain = domainName;
-  if (domainName.length > 3) {
-    redactedDomain = domainName.substring(0, 1) + "*".repeat(domainName.length - 3) + domainName.charAt(domainName.length - 1);
-  } else if (domainName.length > 1) {
-    /*the following line redacts the email between the first and last chars
-    redactedDomain = domainName.charAt(0) + "*".repeat(domainName.length - 2) + domainName.charAt(domainName.length - 1);
-  }*/
-  redactedDomain = domainName.charAt(0) + "*".repeat(domainName.length);
+  // Redacts the domain name.
+  let redactedDomain = domainName;
+  redactedDomain = "*".repeat(domainName.length)
+  // Redacts the tld.
+  let redactedTld = tld;
+  redactedTld = "*".repeat(tld.length - 1);
 
-  return redactedLocal + "@" + redactedDomain + tld;
+  fullRE = "\n-> " + redactedLocal + "@" + redactedDomain + "." + redactedTld;
+
+  optionTwoRE = "\n-> " + localPart.charAt(0) + "*".repeat(Math.max(0, localPart.length - 2)) + localPart.charAt(localPart.length - 1) + "@" + domainName.charAt(0) + "*".repeat(Math.max(0, domainName.length - 2)) + domainName.charAt(domainName.length - 1) + tld;
+
+   optionThreeRE = "\n-> " + localPart.charAt(0) + "*".repeat(Math.max(0, localPart.length - 1)) + "@" + domainName.charAt(0) + "*".repeat(Math.max(0, domainName.length - 1)) + tld;
+
+  totalREOptions = fullRE + optionThreeRE + optionTwoRE;
+  return totalREOptions;
 }
